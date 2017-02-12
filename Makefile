3044bab626b48947e2ae4799ff5ebbbfe1ae7e6b
Andreas Bachmann <bachi@te-clan.ch>
2017-02-12 18:40:06 +0100

    Change Makefile and autogen.mk so that building the executable builds libraries first

--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
 
-LOUD = 1
+LOUD                        = 1
+BUILD                       = debug
 
+### COMPILER AND LINKER FLAGS #################################################
 global_CFLAGS               = -Iinclude -pipe -std=gnu99 \
                               -ffunction-sections -fdata-sections -fms-extensions \
                               -Wall -Wmissing-prototypes -Wno-uninitialized -Wstrict-prototypes
+global_CXXFLAGS             = -Iinclude -pipe \
+                              -ffunction-sections -fdata-sections -fms-extensions \
+                              -Wall -Wno-uninitialized
 global_LDFLAGS              = -g --gc-sections -static
 
+### BUILD TYPES ###############################################################
 debug_CFLAGS                = -ggdb
 debug_LDFLAGS               = 
 
@@ -13,27 +19,35 @@ release_CFLAGS              = -flto -Os
 release_LDFLAGS             = -flto -Os
 
 ### PROGRAMS ##################################################################
-
 PROGRAMS                    = door_lock
 
-### DOOR LOCK #################################################################
 
+### LIBRARIES #################################################################
+LIBRARIES_STATIC 			= cmsis.a
+
+
+### DOOR LOCK #################################################################
 door_lock_LDSCRIPT        	= ldscripts/libs.ld \
                               ldscripts/mem.ld \
                               ldscripts/sections.ld
 door_lock_CFLAGS            = 
 door_lock_LDFLAGS           = 
+door_lock_LIBRARIES         = $(LIBRARIES_STATIC)
 door_lock_SOURCE            = main.c \
                               mfrc522.cpp \
                               bla.c
 
-### LIBRARIES #################################################################
-
-LIBRARIES_STATIC 			= cmsis.a
-
+                              
 ### CMSIS #####################################################################
-
-cmsis.a_SOURCE				= 
+cmsis.a_PATH                = system
+cmsis.a_CFLAGS              = -Isystem/include/stm32f1-stdperiph \
+                              -Isystem/include/cmsis \
+                              -DDEBUG -DUSE_FULL_ASSERT -DTRACE \
+                              -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F10X_MD \
+                              -DUSE_STDPERIPH_DRIVER -DHSE_VALUE=8000000 
+cmsis.a_SOURCE				= stm32f1-stdperiph/misc.c \
+                              stm32f1-stdperiph/stm32f10x_adc.c \
+                              stm32f1-stdperiph/stm32f10x_bkp.c
 
 
 include autogen.mk
--- autogen.mk
+++ autogen.mk
@@ -1,7 +1,7 @@
 
-.SUFFIXES:
+.SUFFIXES::
 
-.PHONY: all clean mkdirs Makefile autogen.mk
+.PHONY:: all clean mkdirs Makefile autogen.mk
 
 CPU                 = cortex-m3
 CODE_GEN            = thumb
@@ -10,9 +10,11 @@ COMPILER_PREFIX     = arm-none-eabi
 CC                  = ${COMPILER_PREFIX}-gcc
 CXX                 = ${COMPILER_PREFIX}-g++
 LD                  = ${COMPILER_PREFIX}-ld
+AR                  = ${COMPILER_PREFIX}-ar
+RANLIB              = ${COMPILER_PREFIX}-ranlib
 OBJCOPY             = ${COMPILER_PREFIX}-objcopy
 OBJDUMP             = ${COMPILER_PREFIX}-objdump
-BUILD               = debug release
+BUILD_TYPES         = debug release
 ARCH                = -m$(CODE_GEN) -mcpu=$(CPU)
 
 LIB_GCC_PATH        = ${shell ${CC} ${ARCH} ${CFLAGS} -print-libgcc-file-name}
@@ -25,6 +27,7 @@ global_LDFLAGS     += $(LIBM_PATH) $(LIBC_PATH) $(LIB_GCC_PATH)
 OBJ_DIR             = obj
 SRC_DIR             = src
 BIN_DIR             = bin
+LIB_DIR             = lib
 
 ELF_SUFFIX          = .elf
 HEX_SUFFIX          = .hex
@@ -63,19 +66,16 @@ endif
 # $(dir filename)               Extracts the directory-part of each filename
 # $(addprefix prefix,names)     Add prefix to names, ex. $(addprefix src/,foo bar)
 #                               Result: src/foo src/bar
-# 
-
-### FIRST TARGET: ALL #########################################################
-all:
-	@echo Use 'make debug' or 'make release'
+#
 
 ### MKDIR FOR EVERY PROGRAM ###################################################
 # $(1) = build type, ex. debug, release
 # $(2) = program name
 
 define DIRECTORY_template
+#--- DIRECTORY ---------------------------------------------------------------
 
-DIRECTORIES += $(OBJ_DIR)/$(1)/$(2)
+$(1)_DIRECTORIES += $(OBJ_DIR)/$(1)/$(2)
 
 $(OBJ_DIR)/$(1)/$(2):
 	@echo "[MKDIR] $$@"
@@ -83,11 +83,12 @@ $(OBJ_DIR)/$(1)/$(2):
 
 endef
 
-### SOURCE FOR EVERY PROGRAM ##################################################
+### SOURCE ####################################################################
 # $(1) = build type, ex. debug, release
 # $(2) = program name
 
 define SOURCE_template
+#--- SOURCE -------------------------------------------------------------------
 
 $(1)_$(2)_C_SOURCE   = $(filter %.$(C_SOURCE_FORMAT),$($(2)_SOURCE))
 $(1)_$(2)_CXX_SOURCE = $(filter %.$(CXX_SOURCE_FORMAT),$($(2)_SOURCE))
@@ -95,11 +96,12 @@ $(1)_$(2)_CXX_SOURCE = $(filter %.$(CXX_SOURCE_FORMAT),$($(2)_SOURCE))
 endef
 
 
-### OBJECT FOR EVERY PROGRAM ##################################################
+### OBJECT ####################################################################
 # $(1) = build type, ex. debug, release
 # $(2) = program name
 
 define OBJECT_template
+#--- OBJECT -------------------------------------------------------------------
 
 $(1)_$(2)_C_OBJECT   = $(addprefix obj/$(1)/$(2)/,$($(1)_$(2)_C_SOURCE:%.$(C_SOURCE_FORMAT)=%.o))
 $(1)_$(2)_CXX_OBJECT = $(addprefix obj/$(1)/$(2)/,$($(1)_$(2)_CXX_SOURCE:%.$(CXX_SOURCE_FORMAT)=%.o))
@@ -109,12 +111,13 @@ $(foreach folder,$(addprefix $(2)/,$(sort $(dir $($(2)_SOURCE)))),$(eval $(call 
 
 endef
 
-### COMPILE FOR EVERY SOURCE FILE ##############################################
+### COMPILE FOR EVERY SOURCE FILE #############################################
 # $(1) = build type, ex. debug, release
 # $(2) = program name
 # $(3) = source directory
 # $(4) = one single source file
 # $(5) = specific CFLAGS
+
 
 #(info C_COMPILE)
 #(info build type:             $(1))
@@ -125,18 +128,48 @@ endef
 #(info .)
 
 define C_COMPILE_template
+#--- C COMPILE ----------------------------------------------------------------
 
-obj/$(1)/$(2)/$($(4):%.$(C_SOURCE_FORMAT)=%.o): obj/$(1)/$(2)/$(dir $($(4))) $(3)/$($(4))
+$(OBJ_DIR)/$(1)/$(2)/$($(4):%.$(C_SOURCE_FORMAT)=%.o): $(3)/$($(4))
 	@echo "[CC   ] $(3)/$($(4))"
 	$(SILENT)$(CC) $(ARCH) -o $$@ -c $(3)/$($(4)) $(global_CFLAGS) $($(5)) $($(2)_CFLAGS)
 
 endef
 
 define CXX_COMPILE_template
+#--- CXX COMPILE --------------------------------------------------------------
 
-obj/$(1)/$(2)/$($(4):%.$(CXX_SOURCE_FORMAT)=%.o): obj/$(1)/$(2)/$(dir $($(4))) $(3)/$($(4))
+$(OBJ_DIR)/$(1)/$(2)/$($(4):%.$(CXX_SOURCE_FORMAT)=%.o): $(3)/$($(4))
 	@echo "[CXX  ] $(3)/$($(4))"
 	$(SILENT)$(CXX) $(ARCH) -o $$@ -c $(3)/$($(4)) $(global_CFLAGS) $($(5)) $($(2)_CFLAGS)
+
+endef
+
+### LIBRARY ###################################################################
+# $(1) = build type, ex. debug, release
+# $(2) = library name
+# $(3) = source directory
+# $(4) = one single source file
+# $(5) = specific CFLAGS
+
+define LIBRARY_template
+#--- LIBRARY ------------------------------------------------------------------
+
+.PHONY:: $(1)_$(2) $(1)_$(2)_clean
+
+$(foreach source,$($(1)_$(2)_C_SOURCE),$(eval $(call C_COMPILE_template,$(1),$(2),$($(2)_PATH)/$(SRC_DIR),source,$(4))))
+$(foreach source,$($(1)_$(2)_CXX_SOURCE),$(eval $(call CXX_COMPILE_template,$(1),$(2),$($(2)_PATH)/$(SRC_DIR),source,$(4))))
+
+$(1)_$(2): $(OBJ_DIR)/$(1)/$(2)/$(2)
+$(OBJ_DIR)/$(1)/$(2)/$(2): $($(1)_$(2)_C_OBJECT) $($(1)_$(2)_CXX_OBJECT)
+	@echo "[AR] $$@"
+	$(SILENT)$(AR) rc $$@ $($(1)_$(2)_C_OBJECT) $($(1)_$(2)_CXX_OBJECT)
+	@echo "[RANLIB] $$@"
+	$(SILENT)$(RANLIB) $$@
+
+$(1)_$(2)_clean:
+	@echo "[CLEAR $(1)]"
+	$(SILENT)rm -rf $($(1)_OBJECT) $(OBJ_DIR)/$(1)
 
 endef
 
@@ -148,15 +181,22 @@ endef
 # $(5) = specific LDFLAGS
 
 define PROGRAM_template
+#--- PROGRAM ------------------------------------------------------------------
+
+.PHONY:: $(1)_$(2) $(1)_$(2)_clean
+
+###
+# ex. hello_LIBRARIES = libnet.a
 
 $(foreach source,$($(1)_$(2)_C_SOURCE),$(eval $(call C_COMPILE_template,$(1),$(2),$(SRC_DIR),source,$(4))))
 $(foreach source,$($(1)_$(2)_CXX_SOURCE),$(eval $(call CXX_COMPILE_template,$(1),$(2),$(SRC_DIR),source,$(4))))
 
-$(3)/$(1)/$(2)$(ELF_SUFFIX): $($(1)_$(2)_C_OBJECT) $($(1)_$(2)_CXX_OBJECT)
-	@mkdir -p $(3)/$(1)
+$(3)/$(1)/$(2)$(ELF_SUFFIX): $(foreach lib, $($(2)_LIBRARIES), $(OBJ_DIR)/$(1)/$(lib)/$(lib)) $($(1)_$(2)_C_OBJECT) $($(1)_$(2)_CXX_OBJECT)
+	@echo "[MKDIR] $(3)/$(1)"
+	$(SILENT)mkdir -p $(3)/$(1)
 	
 	@echo "[LD   ] $(3)/$(1)/$(2)$(ELF_SUFFIX)"
-	$(SILENT)$(LD) $(addprefix -T ,$($(2)_LDSCRIPT)) -o $(3)/$(1)/$(2)$(ELF_SUFFIX) $($(1)_$(2)_C_OBJECT) $($(1)_$(2)_CXX_OBJECT) $(global_LDFLAGS) $($(5)) $($(2)_LDFLAGS)
+	$(SILENT)$(LD) $(addprefix -T ,$($(2)_LDSCRIPT)) -o $(3)/$(1)/$(2)$(ELF_SUFFIX) $($(1)_$(2)_C_OBJECT) $($(1)_$(2)_CXX_OBJECT) $($(1)_$(2)_LIBRARIES:%=-L$(OBJ_DIR)/%) $($(1)_$(2)_LIBRARIES:lib%.a=-l%) $(global_LDFLAGS) $($(5)) $($(2)_LDFLAGS)
 	
 	@echo "[HEX  ] $(3)/$(1)/$(2)$(HEX_SUFFIX)"
 	$(SILENT)$(OBJCOPY) $(OBJCOPY_HEX_FLAGS) $(3)/$(1)/$(2)$(ELF_SUFFIX) $(3)/$(1)/$(2)$(HEX_SUFFIX)
@@ -169,30 +209,61 @@ $(3)/$(1)/$(2)$(ELF_SUFFIX): $($(1)_$(2)_C_OBJECT) $($(1)_$(2)_CXX_OBJECT)
 
 $(1)_$(2)_clean:
 	@echo "[CLEAR $(2)]"
-	@$(SILENT)rm -rf $(2) $($(1)_$(2)_C_OBJECT) $($(1)_$(2)_CXX_OBJECT) obj/$(2)
+	$(SILENT)rm -rf $(2) $($(1)_$(2)_C_OBJECT) $($(1)_$(2)_CXX_OBJECT) obj/$(2)
 
 endef
 
-### BUILD #####################################################################
+### BUILD TYPE ################################################################
 # $(1) = build type, ex. debug, release
 
-define BUILD_template
+define BUILD_TYPE_template
+#--- BUILD --------------------------------------------------------------------
+
+$(1)_print:
+	@echo "Using 'make $(1)'"
 
 # add prefix, ex. bin/debug to all PROGRAMS: prog.elf
-$(1): $(foreach prog, $(PROGRAMS), $(addprefix $(BIN_DIR)/$(1),/$(prog)$(ELF_SUFFIX)))
+$(1): $(1)_print $(1)_mkdirs $(foreach prog, $(PROGRAMS), $(addprefix $(BIN_DIR)/$(1),/$(prog)$(ELF_SUFFIX)))
 	@echo [DONE ]
 
+###
+# ex.
+# LIBRARIES_STATIC = libnet.a
+# libnet.a_SOURCE  = net.c dma.c int.c
+# $(lib) => lib.a
+$(foreach lib,  $(LIBRARIES_STATIC), $(eval $(call SOURCE_template,$(1),$(lib),$(addprefix $(1),_CFLAGS))))
+$(foreach lib,  $(LIBRARIES_STATIC), $(eval $(call OBJECT_template,$(1),$(lib),$(addprefix $(1),_CFLAGS))))
+
+$(foreach lib,  $(LIBRARIES_STATIC), $(eval $(call LIBRARY_template,$(1),$(lib))))
+
+###
+# ex.
+# PROGRAMS     = hello
+# hello_SOURCE = main.c print.c calc.c
+# $(prog) => hello
 $(foreach prog, $(PROGRAMS), $(eval $(call SOURCE_template,$(1),$(prog),$(addprefix $(1),_CFLAGS))))
 $(foreach prog, $(PROGRAMS), $(eval $(call OBJECT_template,$(1),$(prog),$(addprefix $(1),_CFLAGS))))
-$(foreach prog, $(PROGRAMS), $(eval $(call PROGRAM_template,$(1),$(prog),bin,$(addprefix $(1),_CFLAGS),$(addprefix $(1),_LDFLAGS))))
 
+$(foreach prog, $(PROGRAMS), $(eval $(call PROGRAM_template,$(1),$(prog),$(BIN_DIR),$(addprefix $(1),_CFLAGS),$(addprefix $(1),_LDFLAGS))))
+
+###
+# debug_DIRECTORIES is populated after calling OBJECT_template!
+$(1)_mkdirs: $($(1)_DIRECTORIES)
 
 endef
 
 ### MAIN ######################################################################
-# Iterate over BUILD: debug, release
 
-$(foreach build, $(BUILD), $(eval $(call BUILD_template,$(build))))
+###
+# Use the default BUILD, ex. debug
+all: $(BUILD)
+
+###
+# Iterate over BUILD_TYPES: debug, release
+$(foreach build, $(BUILD_TYPES), $(eval $(call BUILD_TYPE_template,$(build))))
+
+$(info $(debug_cmsis.a_C_OBJECT))
+$(info $(debug_cmsis.a_CXX_OBJECT))
 
 clean:
 	@echo "[CLEAN]"
